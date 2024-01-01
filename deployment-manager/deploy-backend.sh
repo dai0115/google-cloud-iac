e2402bf3f04d65b0b409dd3361672e86cc2e96fe
dai0115 <hateen0808@gmail.com>
2024-01-01 15:22:51 +0900

    [fix]modified backend bucket config

--- deployment-manager/deploy-backend.sh
+++ deployment-manager/deploy-backend.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 gcloud deployment-manager deployments create terrform-backend --config gcs.yml
+# gcloud deployment-manager deployments update terrform-backend --config gcs.yml
+# gcloud deployment-manager deployments delete terrform-backend
